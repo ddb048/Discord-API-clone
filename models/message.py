0009baac504ca8af396e0fa9e0225a7f077b72a4
Moran <method.even@gmail.com>
2022-11-09 15:45:34 -0800

    models and relationships update

---
+++ models/message.py
@@ -0,0 +1,14 @@
+from .db import db
+from sqlalchemy.sql import func
+
+class Massege(db.Model):
+    __tablename__ = 'masseges'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    is_voice = db.Column(db.Boolean, nullable=False)
+    description = db.Column(db.String(2000))
+    server_id = db.Column(db.Integer, db.ForeignKey('servers.id'))
+    created_at = db.Column(db.DateTime(),nullable = False ,server_default = func.now())
+    updated_at = db.Column(db.DateTime(), nullable = False, onupdate = func.now(), default = func.now())
+ #relatioship
+    server = db.relationship('Server', back_populate='masseges')
