0eae8cc5bc9582cc55056aab9358f1d8ab018417
abel <abrianvil@gmail.com>
2022-11-10 00:26:13 -0500

    grammar fixes

--- models/channel.py
+++ models/channel.py
@@ -8,10 +8,8 @@ class Channel(db.Model):
     name = db.Column(db.String, nullable = False)
     is_voice = db.Column(db.Boolean, nullable = False)
     description = db.Column(db.String(2000))
-    server_id = db.Column(db.Integer, forein_key = True)
+    server_id = db.Column(db.Integer, foreign_key = True)
     created_at = db.Column(db.DateTime(), nullable=False,server_default=func.now())
     updated_at = db.Column(db.DateTime(), nullable=False,onupdate=func.now(), default=func.now())
- #relatioship
+ #relationship
     server = db.relationship('Server', back_populate = 'servers')
-
-    
--- models/server.py
+++ models/server.py
@@ -11,9 +11,8 @@ class Server(db.Model):
     preview_image = db.Column(db.String)
     private = db.Column(db.Boolean)
     is_DM = db.Column(db.Boolean)
-    owner_id = db.Column(db.Integer, forein_key=True)
- #relatioship
+    owner_id = db.Column(db.Integer, foreign_key=True)
+ #relationship
     members = db.relationship('Member', back_populate='servers', cascade="all,delete-orphan")
-    massege = db.relationship('Massege', back_populate='server', cascade="all,delete")
+    message = db.relationship('Message', back_populate='server', cascade="all,delete")
     channel = db.relationship('Channel', back_populate='server', cascade="all,delete")
-
