b856f106ec42e17b1c7133caa892fae5a5bf6f10
david <ddb048@gmail.com>
2022-11-11 14:03:05 -0800

    models update

--- app/models/server.py
+++ app/models/server.py
@@ -7,10 +7,11 @@ class Server(db.Model):
     __tablename__ = 'servers'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
+    name = db.Column(db.String(100), unique=True)
     preview_image = db.Column(db.String(255))
     private = db.Column(db.Boolean)
     is_DM = db.Column(db.Boolean)
+    server_description = db.Column(db.String(255))
     owner_id = db.Column(db.Integer, foreign_key=True)
     created_at = db.Column(db.DateTime(), nullable=False,server_default=func.now())
     updated_at = db.Column(db.DateTime(), nullable=False,onupdate=func.now(), default=func.now())
