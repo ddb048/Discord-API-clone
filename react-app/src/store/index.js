2e5638bc343f938d1fc782b7020bfdc02b867f0b
abel <abrianvil@gmail.com>
2022-11-11 13:20:36 -0500

    frontend setup

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from './session'
 
 const rootReducer = combineReducers({
     session: sessionReducer
