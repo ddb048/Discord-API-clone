8c3f7f30f496c8ec712113a69a8b5edd4ac5ad8d
abel <abrianvil@gmail.com>
2022-11-13 02:45:09 -0500

    message form refactored

--- app/forms/new_message.py
+++ app/forms/new_message.py
@@ -4,7 +4,6 @@ from wtforms import StringField, BooleanField, IntegerField, TextAreaField
 from flask_wtf import FlaskForm
 
 
-
 # TODO - inquire about if we want channel and server id to be required
 class New_message(FlaskForm):
     message_body = TextAreaField('Message', validators=[DataRequired()])
