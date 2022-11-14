e52693f302c50d982ae825707e30ef0d70257710
david <ddb048@gmail.com>
2022-11-14 08:28:55 -0800

    updated bug fixes from render

--- react-app/src/components/Navbar/index.js
+++ react-app/src/components/Navbar/index.js
@@ -1,41 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import LogoutButton from '../auth/LogoutButton';
 import './NavBar.css'
 const NavBar = () => {
   return (
     <div className='splash-navbar-container'>
-    <nav>
-      <div className='splash-nav-box'>
-        <div className='splash-logo-left'>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Q-CORD
-          </NavLink>
+      <nav>
+        <div className='splash-nav-box'>
+          <div className='splash-logo-left'>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              Q-CORD
+            </NavLink>
+          </div>
+          <div className='splash-mid-links'>
+            <div>
+              <NavLink to='/login' exact={true} activeClassName='active'>
+                Login
+              </NavLink>
+            </div>
+            <div>
+              <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                Sign Up
+              </NavLink>
+            </div>
+            <div>
+              <NavLink to='/discover' exact={true} activeClassName='active'>
+                Discover
+              </NavLink>
+            </div>
+          </div>
+          <div className='splash-logout-right'>
+            <div>
+              <LogoutButton />
+            </div>
+          </div>
         </div>
-        <div className='splash-mid-links'>
-        <div>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </div>
-        <div>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </div>
-        <div>
-          <NavLink to='/discover' exact={true} activeClassName='active'>
-            Discover
-          </NavLink>
-        </div>
-        </div>
-        <div className='splash-logout-right'>
-        <div>
-          <LogoutButton />
-        </div>
-        </div>
-      </div>
-    </nav>
+      </nav>
     </div>
   );
 }
