a6dfb82e450252c4c6bef8f04d93dad82e736a8c
david <ddb048@gmail.com>
2022-11-13 19:56:48 -0800

    server reducer added

--- react-app/src/components/Discover/index.js
+++ react-app/src/components/Discover/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import { useSelector } from 'react-redux'
 
 function Discover() {
-    const servers = useSelector(state, console.log(state.servers))
-    return render (
-
+    const servers = useSelector(state => state.servers)
+    return (
+        <h1>Discovery Page</h1>
     )
 }
 
