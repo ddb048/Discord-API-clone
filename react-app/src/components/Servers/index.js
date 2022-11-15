1af1e45aa5200357812c0d20fbf9c39ecf41dcc5
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2022-11-14 18:40:23 -0800

    Refactored all channels, all messages, all servers to remove duplicated data that was not inside the keys

--- react-app/src/components/Servers/index.js
+++ react-app/src/components/Servers/index.js
@@ -3,21 +3,25 @@ import { NavLink, useHistory, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllCurrentUserServers } from '../../store/servers';
 import { getServerDetails } from '../../store/servers';
+import { getAllChannel } from '../../store/channel';
+import { getAllMessages } from '../../store/message';
 import './Servers.css';
 
 const Servers = () => {
 	const dispatch = useDispatch();
 	const history = useHistory();
-  // grabbing the state of servers in servers
+	// grabbing the state of servers in servers
 	const servers = useSelector((state) => Object.values(state.servers.servers));
-// grabbing the state of channels in channels
-// const channels = useSelector((state) => state.)
+	// grabbing the state of channels in channels
+	const channels = useSelector((state) => state.channels.channels);
+	console.log('channels useSelector >>>>', channels);
 	const { serverId } = useParams();
-// dispatching to the current user's servers
+	// dispatching to the current user's servers
 	useEffect(() => {
 		dispatch(getAllCurrentUserServers());
+		dispatch(getAllChannel(10));
 	}, [dispatch]);
-  // I was servers so this was my workaround
+	// I was servers so this was my workaround
 	// let serverArr;
 	// if (servers) {
 	// 	serverArr = Object.values(servers);
@@ -26,15 +30,19 @@ const Servers = () => {
 
 	return (
 		<div className="servers-page-container">
-			<div className="servers-column-container" >
-				<h1 className='test-name'>servers</h1>
+			<div className="servers-column-container">
+				<h1 className="test-name">servers</h1>
 				{servers.map((server) => {
 					return (
 						<div className="servers-button-map" key={server.name}>
 							<NavLink to={`servers/${server.id}`}>
 								<div className="servers-photo-container">
 									<div>
-										<img className="servers-photo" src={server.preview_image} alt="server img" />
+										<img
+											className="servers-photo"
+											src={server.preview_image}
+											alt="server img"
+										/>
 									</div>
 								</div>
 							</NavLink>
@@ -43,21 +51,23 @@ const Servers = () => {
 				})}
 			</div>
 			<div className="servers-channels-container">
-      {servers.map((server) =>{
-        return(
-          <div key={server.channel}>
-
-          </div>
-        )
-      })}
-				<h1 className='test-name'>channels</h1>
+					<ul>
+				{/* {channels.map((channel) => {
+						return (
+						<div>
+							<li className='servers-channel-name' key={channel.id}>{channel.name}</li>
+						</div>
+						);
+				})} */}
+					</ul>;
+				<h1 className="test-name">channels</h1>
 			</div>
-      <div className='servers-messages-container'>
-        <h1 className='test-name'>messages section</h1>
-      </div>
-      <div className='servers-active-container'>
-        <h1 className='test-name'>active section</h1>
-      </div>
+			<div className="servers-messages-container">
+				<h1 className="test-name">messages section</h1>
+			</div>
+			<div className="servers-active-container">
+				<h1 className="test-name">active section</h1>
+			</div>
 		</div>
 	);
 };
