444eeff0a34b6217dd365edcd18bf8c11c8d1490
david <ddb048@gmail.com>
2022-11-16 09:40:48 -0800

    updated bug fixes

--- react-app/src/components/OnePageServer/index.js
+++ react-app/src/components/OnePageServer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react'
-import { useParams, NavLink} from 'react-router-dom'
+import { useParams, NavLink } from 'react-router-dom'
 import { getServerDetails } from "../../store/servers";
 import splash_3 from '../../Images/discord-splash-3.png';
 import fun from '../../Images/fun.png'
@@ -14,97 +14,97 @@ import "./onePage.css";
 
 
 function OnePageServer() {
-// const servers = useSelector((state) => state.servers.servers);
-const oneServer = useSelector(state=>state.servers.oneServer);
-console.log("ONE PAGE SERVERS STATE", oneServer);
-const dispatch = useDispatch();
-let { serverId } = useParams();
+  // const servers = useSelector((state) => state.servers.servers);
+  const oneServer = useSelector(state => state.servers.oneServer);
+  console.log("ONE PAGE SERVERS STATE", oneServer);
+  const dispatch = useDispatch();
+  let { serverId } = useParams();
 
 
-useEffect(() => {
-  // dispatch(getAllServers());
-  dispatch(getServerDetails(+serverId))
-}, [dispatch]);
+  useEffect(() => {
+    // dispatch(getAllServers());
+    dispatch(getServerDetails(+serverId))
+  }, [dispatch, serverId]);
 
-console.log('ONE SERVER', oneServer)
+  console.log('ONE SERVER', oneServer)
 
-if (!oneServer) return null
-else
-return (
-  <>
-    {/* NOTE upper (blue) div with pic and links */}
-    <div id="upper">
-      <div id="header-links">
-        <NavLink id="links" to="/discover">
-          Discover
-        </NavLink>
+  if (!oneServer) return null
+  else
+    return (
+      <>
+        {/* NOTE upper (blue) div with pic and links */}
+        <div id="upper">
+          <div id="header-links">
+            <NavLink id="links" to="/discover">
+              Discover
+            </NavLink>
 
-        <NavLink id="links" to="/safety">
-          Safety
-        </NavLink>
-      </div>
-      <img id="upper-img" src={oneServer.preview_image} alt='upper-img'/>
-    </div>
-    {/* NOTE lower part white */}
-    <div id="lower-div">
-      <h1 id="bold-title">{oneServer.name}</h1>
-      <div id="sub-title">{oneServer.num_member} members</div>
-
-      <div id="content-container">
-        <div id="in-with-img">
-          <img id="bullatin-img" src={calender} alt='img' />
-          <p>{oneServer.created_at}</p>
+            <NavLink id="links" to="/safety">
+              Safety
+            </NavLink>
+          </div>
+          <img id="upper-img" src={oneServer.preview_image} alt='upper-img' />
         </div>
-        <div id="in-with-img">
-          <img id="bullatin-img" src={fun} alt='img'/>
-          <p>Have fun discussions with other members.</p>
-        </div>
-        <div id="in-with-img">
-          <img id="bullatin-img" src={games} alt='img' />
-          <p>Meet new people to play the game with</p>
-        </div>
-        <div id="in-with-img">
-          <img id="bullatin-img" src={exlamation} alt='img' />
-          <p>Post your beautiful fanart and suggestions</p>
-        </div>
-        <div id="in-with-img">
-          <img id="bullatin-img" src={chaty} alt='img'/>
-          <p>
-            Find trades, raids, and many other game events going on to complete
-            with friends.
-          </p>
-        </div>
-      </div>
-    </div>
-    {/* NOTE lower part with pic and join button, reffer to css from splash page */}
-    <div id="bold-title">About</div>
-    <div id="text">{oneServer.description}</div>
+        {/* NOTE lower part white */}
+        <div id="lower-div">
+          <h1 id="bold-title">{oneServer.name}</h1>
+          <div id="sub-title">{oneServer.num_member} members</div>
 
-    <div className="details-container">
-      <div className="details-card">
-        <img className="details-card-photo" src={splash_3} alt="img" />
-        <div className="ddetails-card-text">
-          <div className="sub-title">Where Hanging Out is Easy</div>
-          <div className="sub-subtext">
-            Grab a seat in a voice channel when youre free. Friends in your
-            server can see youre around and instantly pop in to talk without
-            having to call.
+          <div id="content-container">
+            <div id="in-with-img">
+              <img id="bullatin-img" src={calender} alt='img' />
+              <p>{oneServer.created_at}</p>
+            </div>
+            <div id="in-with-img">
+              <img id="bullatin-img" src={fun} alt='img' />
+              <p>Have fun discussions with other members.</p>
+            </div>
+            <div id="in-with-img">
+              <img id="bullatin-img" src={games} alt='img' />
+              <p>Meet new people to play the game with</p>
+            </div>
+            <div id="in-with-img">
+              <img id="bullatin-img" src={exlamation} alt='img' />
+              <p>Post your beautiful fanart and suggestions</p>
+            </div>
+            <div id="in-with-img">
+              <img id="bullatin-img" src={chaty} alt='img' />
+              <p>
+                Find trades, raids, and many other game events going on to complete
+                with friends.
+              </p>
+            </div>
           </div>
         </div>
-        <NavLink className="details-card-bttn" to="/sign-up">
-          <div>Join Q-Core</div>
-        </NavLink>
-      </div>
-    </div>
+        {/* NOTE lower part with pic and join button, reffer to css from splash page */}
+        <div id="bold-title">About</div>
+        <div id="text">{oneServer.description}</div>
 
-    {/* FIXME we need to implament the righ url for joining a server */}
-    <div id="footer-div">
-      <NavLink id="footer" to={`/servers/${serverId}`}>
-        <div id="in-footer">Join Server</div>
-      </NavLink>
-    </div>
-  </>
-);
+        <div className="details-container">
+          <div className="details-card">
+            <img className="details-card-photo" src={splash_3} alt="img" />
+            <div className="ddetails-card-text">
+              <div className="sub-title">Where Hanging Out is Easy</div>
+              <div className="sub-subtext">
+                Grab a seat in a voice channel when youre free. Friends in your
+                server can see youre around and instantly pop in to talk without
+                having to call.
+              </div>
+            </div>
+            <NavLink className="details-card-bttn" to="/sign-up">
+              <div>Join Q-Core</div>
+            </NavLink>
+          </div>
+        </div>
+
+        {/* FIXME we need to implament the righ url for joining a server */}
+        <div id="footer-div">
+          <NavLink id="footer" to={`/servers/${serverId}`}>
+            <div id="in-footer">Join Server</div>
+          </NavLink>
+        </div>
+      </>
+    );
 
 
 
---
+++ react-app/src/components/SafetyPage/index.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { NavLink } from 'react-router-dom'
+import hummer from '../../Images/safety-hummer.jpeg'
+import yellow from '../../Images/safety-yellow.jpeg'
+import teens from '../../Images/safety-teens.jpeg'
+import shield from '../../Images/safety-shield.jpeg'
+import tools from '../../Images/safety-tools.jpeg'
+import './safety.css'
+
+
+
+function Safety() {
+
+  return (
+    <>
+      <div id="s-upper">
+        <div id="header-links">
+          <NavLink id="links" to="/discover">
+            Discover
+          </NavLink>
+
+          <NavLink id="links" to="/login">
+            Q-cord
+          </NavLink>
+        </div>
+        <div id="h1"> DISCORD SAFETY CENTER</div>
+        <div id="h2">what is Q-cord?</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div id="h2">Q-cord commitment to a safe and trusted experience</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div id="middle">
+        <div id="general">
+          <div id="left">
+            <div id="left-title">SAFETY</div>
+            <img src={tools} alt="" />
+            <div id="left-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam.
+            </div>
+          </div>
+          <div id="right">
+            <div id="a">
+              <img id="safety-img" src={hummer} alt="" />
+              <div id="blue-title">reprehenderit in voluptate</div>
+              <div id="black-title">Excepteur sint occaecat</div>
+            </div>
+            <div id="b">
+              <img id="safety-img" src={shield} alt="" />
+              <div id="blue-title">voluptate velit esse </div>
+              <div id="black-title"> mollit anim id est</div>
+            </div>
+            <div id="c">
+              <img id="safety-img" src={teens} alt="" />
+              <div id="blue-title">cupidatat non proident</div>
+              <div id="black-title">voluptate velit esse </div>
+            </div>
+
+            <div id="d">
+              <img id="safety-img" src={yellow} alt="" />
+              <div id="blue-title">ipsum dolor sit amet</div>
+              <div id="black-title">quis nostrud exercitation</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Safety
