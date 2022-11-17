b29efd0e57fcf5b0907284e24b65f6f4a306d8a1
david <ddb048@gmail.com>
2022-11-17 10:41:35 -0800

    render bugs

--- react-app/src/components/ServerDetails/NewChannelModal.js
+++ react-app/src/components/ServerDetails/NewChannelModal.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { createChannel } from '../../store/channel';
 // import {
 // 	getServerDetails,
