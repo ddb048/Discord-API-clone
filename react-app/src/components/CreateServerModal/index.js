998da36b43bb51aa10743e0b932817f7e972e91a
Moran <method.even@gmail.com>
2022-11-16 21:15:28 -0800

    merge dev into discover

--- react-app/src/components/CreateServerModal/index.js
+++ react-app/src/components/CreateServerModal/index.js
@@ -1,22 +1,3 @@
-import React, { useState } from 'react';
-import { Modal } from '../../context/Modal';
-
-function CreateServerFormModal() {
-    const [showModal, setShowModal] = useState(false);
-
-    return (
-        <>
-            <div className='create-server-button' onClick={() => setShowModal(true)}>Log In</div>
-            {showModal && (
-                <Modal onClose={() => setShowModal(false)}>
-                    <div>Create Server Form</div>
-                </Modal>
-            )}
-        </>
-    );
-}
-
-export default CreateServerFormModal;
 import React, { useState } from "react";
 import { Modal } from "../../context/Modal";
 import CreateServerForm from "../CreateServerForm";
