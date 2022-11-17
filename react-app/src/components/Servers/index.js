f6c983544e64c75530e112d61e666ef12d123334
david <ddb048@gmail.com>
2022-11-17 08:48:27 -0800

    updated server

--- react-app/src/components/Servers/index.js
+++ react-app/src/components/Servers/index.js
@@ -6,13 +6,11 @@ import { getAllCurrentUserServers } from '../../store/servers';
 import { getAllMembers } from '../../store/member';
 import { getChannelDetail } from '../../store/channel';
 import LogoutButton from '../auth/LogoutButton';
-import CreateServerFormModal from '../CreateServerModal';
 import CreateServerForm from '../CreateServerForm';
 // import { getAllMessages } from '../../store/message';
 import { getServerDetails } from '../../store/servers';
 import DM_button from '../../Images/q-cord-button.png';
 import './Servers.css';
-import CreateServerModal from '../CreateServerModal';
 
 
 
